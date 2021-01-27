dd122a7267828f024ba960fa1e0c46c2dedb05ac
hendhelal <37216323+hendhelal@users.noreply.github.com>
2021-01-27 16:06:16 +0200

    path_1-stopPoints

---
+++ tarffic System/Assets/Scripts/CreatePath.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreatePath : MonoBehaviour
+{
+    public Color pathColor =Color.red;
+    public List<Transform> pathPoints = new List<Transform>();
+    Transform[] pathPointsArray;
+   
+    void OnDrawGizmos()
+    {
+        Gizmos.color = pathColor;
+        pathPointsArray = GetComponentsInChildren<Transform>();
+        pathPoints.Clear();
+        foreach (var item in pathPointsArray)
+        {
+            if(item!= this.transform)
+            {
+                pathPoints.Add(item);
+            }
+        }
+        for (int i = 0; i < pathPoints.Count; i++)
+        {
+            Vector3 position = pathPoints[i].position;
+            if (i > 0)
+            {
+                Vector3 previous = pathPoints[i - 1].position;
+                Gizmos.DrawLine(previous,position);
+                Gizmos.DrawWireSphere(position, 0.3f);
+            }
+
+        }
+        
+    }
+}
---
+++ tarffic System/Assets/Scripts/MovingCarBehavior.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingCarBehavior : MonoBehaviour
+{
+    public CreatePath roadPath;
+    public int currentwayPointIndex = 0;
+    public float speed;
+    public float reachDistance = 1.0f;
+    public float rotaionSpeed = 5.0f;
+    public string PathName;
+    public bool move = true;
+    Vector3 lastPos,currentPos;
+    
+    
+    void Start()
+    {
+       // roadPath = GameObject.Find(PathName).GetComponent<CreatePath>();
+        lastPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(move)
+        {
+            float distance = Vector3.Distance(roadPath.pathPoints[currentwayPointIndex].position, transform.position);
+            transform.position = Vector3.MoveTowards(transform.position, roadPath.pathPoints[currentwayPointIndex].position, Time.deltaTime * speed);
+            var rotation = Quaternion.LookRotation(roadPath.pathPoints[currentwayPointIndex].position - transform.position);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * rotaionSpeed);
+            if (distance < reachDistance)
+                currentwayPointIndex++;
+            if (currentwayPointIndex >= roadPath.pathPoints.Count)
+                currentwayPointIndex = 0;
+        }
+      
+    }
+
+   void  OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag=="intersectionPoint")
+          move = false;
+        else
+          speed -= 5;
+ 
+        Debug.Log("coliidee");
+        
+       
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "intersectionPoint")
+            move = true;
+        else
+            speed += 5;
+        Debug.Log("no coliidee");
+
+
+    }
+    IEnumerator wait()
+    {
+        yield return new WaitForSecondsRealtime(1);
+        move = true;
+    }
+}
